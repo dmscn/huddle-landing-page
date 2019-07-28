4c1711c293f9aeffd262f5076178b0ebcd82645d
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-07-28 20:35:58 -0300

    Improved Box props

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,8 +1,18 @@
 import styled from 'styled-components'
 
 const Box = styled.div`
+  box-sizing: border-box;
   display: flex;
-  flex: ${props => props.flexSize};
+  margin-top: ${props =>
+    props.marginTop || props.margin || props.marginVertical};
+  margin-bottom: ${props =>
+    props.marginBottom || props.margin || props.marginVertical};
+  margin-left: ${props =>
+    props.marginLeft || props.margin || props.marginHorizontal};
+  margin-right: ${props =>
+    props.marginRight || props.margin || props.marginHorizontal};
+  padding: ${props => props.padding};
+  flex: ${props => (props.full ? 1 : props.flexSize)};
   flex-direction: ${props => (props.column ? 'column' : 'row')};
   justify-content: ${props => props.justify || 'center'};
   align-items: ${props => props.align || 'center'};
