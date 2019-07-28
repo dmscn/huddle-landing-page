3b9ce4c20f730e6222bec15b26371334bf3af887
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-07-28 20:38:16 -0300

    Refactor <MainContent />

--- src/containers/MainContent/index.js
+++ src/containers/MainContent/index.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import illustrations from '../../assets/images/illustration-mockups.svg'
+import Breackpoint from 'react-socks'
 
-import { Container, Image } from './styled'
+import Box from '../../components/Box'
 import DescriptionBox from '../DescriptionBox'
+
+const PageContent = ({ row }) => (
+  <>
+    <Box full marginRight={row && '2rem'}>
+      <img src={illustrations} alt="Mockup illustrations" width="100%" />
+    </Box>
+    <Box full marginLeft={row && '2rem'}>
+      <DescriptionBox />
+    </Box>
+  </>
+)
 
 export default function MainContent() {
   return (
-    <Container>
-      <Image src={illustrations} alt="Mockup illustrations" />
-      <DescriptionBox />
-    </Container>
+    <Box full>
+      <Breackpoint medium down>
+        <Box full column justify="space-around">
+          <PageContent />
+        </Box>
+      </Breackpoint>
+
+      <Breackpoint large up>
+        <Box full margin="2rem">
+          <PageContent row />
+        </Box>
+      </Breackpoint>
+    </Box>
   )
 }
