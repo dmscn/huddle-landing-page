70c390d937d00a1a44902c213843dd2705d7207c
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-07-28 20:37:52 -0300

    Refactor <SocialNetworks />

--- src/containers/SocialNetworks/index.js
+++ src/containers/SocialNetworks/index.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 import Breackpoint from 'react-socks'
 
 import BrandIcon from '../../components/BrandIcon'
 import Box from '../../components/Box'
 
+const Wrapper = styled.div`
+  width: 100%;
+`
+
 export default function SocialNetworks() {
   const icons = ['facebook-f', 'instagram', 'twitter']
 
   return (
-    <>
+    <Wrapper>
       <Breackpoint medium down>
-        <Box justify="center">
+        <Box justify="center" marginTop="3rem">
           {icons.map(icon => (
             <BrandIcon key={icon} icon={icon} color="white" border />
           ))}
@@ -31,6 +36,6 @@ export default function SocialNetworks() {
           ))}
         </Box>
       </Breackpoint>
-    </>
+    </Wrapper>
   )
 }
