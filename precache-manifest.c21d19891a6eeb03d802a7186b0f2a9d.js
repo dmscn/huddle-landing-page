ab63c7903616aca27a187c26c04fceace08c0948
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-07-28 23:01:58 -0300

    Updates

---
+++ precache-manifest.c21d19891a6eeb03d802a7186b0f2a9d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb70c7fe7df17fa9af00acac17282a7",
+    "url": "/huddle-landing-page/index.html"
+  },
+  {
+    "revision": "d81b3da8ea72ccd252ec",
+    "url": "/huddle-landing-page/static/css/main.f775db06.chunk.css"
+  },
+  {
+    "revision": "c063e9858f6a760f2f7c",
+    "url": "/huddle-landing-page/static/js/2.557bf0d6.chunk.js"
+  },
+  {
+    "revision": "d81b3da8ea72ccd252ec",
+    "url": "/huddle-landing-page/static/js/main.391a8158.chunk.js"
+  },
+  {
+    "revision": "da1f0bc7b9374cbe44ee",
+    "url": "/huddle-landing-page/static/js/runtime~main.2015dbcf.js"
+  },
+  {
+    "revision": "bbf1c96bd230d1d4845f0ffa730f7e35",
+    "url": "/huddle-landing-page/static/media/bg-desktop.bbf1c96b.svg"
+  },
+  {
+    "revision": "3b9d812e096dd718f15a8c95dc19c6ba",
+    "url": "/huddle-landing-page/static/media/bg-mobile.3b9d812e.svg"
+  },
+  {
+    "revision": "129756c283039eb61e2212ce6c7561a2",
+    "url": "/huddle-landing-page/static/media/illustration-mockups.129756c2.svg"
+  },
+  {
+    "revision": "f5d36b374127108f655fa90730782e2f",
+    "url": "/huddle-landing-page/static/media/logo.f5d36b37.svg"
+  }
+]);
